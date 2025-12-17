ca70cbd5028c03641bf4b59a4eb4747fdfcae52b
masshuka <msipicyna96@gmail.com>
2025-12-17 12:19:57 +0500

    Созданный классы для приложения. Isakov

---
+++ TaskManagerTelegramBot/Classes/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerTelegramBot.Classes
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public ICollection<Event> Events { get; set; }
+        public User(Guid userId) 
+        {
+            Id = userId;
+            Events = new ICollection<Event>;
+        }
+    }
+}
